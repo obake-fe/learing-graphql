0c742ec9d40dfe5821dbf2e2a4fc7dac0c09ba3a
obake-fe <daichismash@gmail.com>
2023-04-06 21:17:34 +0900

    feat: add first schema and resolver

---
+++ server/index.ts
@@ -0,0 +1,28 @@
+import {ApolloServer} from "@apollo/server";
+import {startStandaloneServer} from "@apollo/server/standalone";
+
+const typeDefs = `
+	type Query {
+		totalPhotos: Int!		
+	}
+`
+
+const resolvers = {
+	Query: {
+		totalPhotos: () => 42
+	}
+}
+
+// サーバのインスタンスを作成
+// その際、typeDefs(スキーマ)とリゾルバを引数に取る
+const server = new ApolloServer({
+	typeDefs,
+	resolvers
+})
+
+// webサーバを起動
+const { url } = await startStandaloneServer(server, {
+	context: async ({ req }) => ({ token: req.headers.token }),
+	listen: { port: 4000 },
+});
+console.log(`🚀  Server ready at ${url}`);
