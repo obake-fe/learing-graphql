602058528fdd4935b5ecd8bfef28ffc249e868d5
obake-fe <daichismash@gmail.com>
2023-04-07 17:51:27 +0900

    feat: codegenを導入してTypeScriptの型定義ファイルを作成する

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import { loadSchema } from '@graphql-tools/load';
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 import { addResolversToSchema } from '@graphql-tools/schema';
+import { Resolvers } from './types/generated/graphql';
 
 // schema is `GraphQLSchema` instance
 const schema = await loadSchema('schema.graphql', {
@@ -13,24 +14,23 @@ const schema = await loadSchema('schema.graphql', {
 // 写真を格納するための配列を定義する
 let photos = [];
 
-const resolvers = {
+const resolvers: Resolvers = {
   Query: {
     // 写真を格納した配列の長さを返す
     totalPhotos: () => photos.length
-  }
+  },
   // postPhotoミューテーションと対応するリゾルバ
-  // Mutation: {
-  //   postPhoto(parent, args) {
-  //     photos.push(args);
-  //     return true;
-  //   }
-  // }
+  Mutation: {
+    postPhoto(parent, args) {
+      photos.push(args);
+      return true;
+    }
+  }
 };
 
 const schemaWithResolvers = addResolversToSchema({ schema, resolvers });
 
 // サーバのインスタンスを作成
-// その際、typeDefs(スキーマ)とリゾルバを引数に取る
 const server = new ApolloServer({
   schema: schemaWithResolvers
 });
