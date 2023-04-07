b9c65521b212a88961f078f3ed5a202a1a7cabe5
obake-fe <daichismash@gmail.com>
2023-04-07 16:44:44 +0900

    feat: スキーマファイルを分離する

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,14 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { loadSchema } from '@graphql-tools/load';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+import { addResolversToSchema } from '@graphql-tools/schema';
 
-const typeDefs = `
-  type Query {
-    totalPhotos: Int!		
-	}
-	type Mutation {
-	  postPhoto(name: String!, description: String!): Boolean!
-	}
-`;
+// schema is `GraphQLSchema` instance
+const schema = await loadSchema('schema.graphql', {
+  // load from a single schema file
+  loaders: [new GraphQLFileLoader()]
+});
 
 // 写真を格納するための配列を定義する
 let photos = [];
@@ -17,21 +17,22 @@ const resolvers = {
   Query: {
     // 写真を格納した配列の長さを返す
     totalPhotos: () => photos.length
-  },
-  // postPhotoミューテーションと対応するリゾルバ
-  Mutation: {
-    postPhoto(parent, args) {
-      photos.push(args);
-      return true;
-    }
   }
+  // postPhotoミューテーションと対応するリゾルバ
+  // Mutation: {
+  //   postPhoto(parent, args) {
+  //     photos.push(args);
+  //     return true;
+  //   }
+  // }
 };
+
+const schemaWithResolvers = addResolversToSchema({ schema, resolvers });
 
 // サーバのインスタンスを作成
 // その際、typeDefs(スキーマ)とリゾルバを引数に取る
 const server = new ApolloServer({
-  typeDefs,
-  resolvers
+  schema: schemaWithResolvers
 });
 
 // webサーバを起動
